704eeee4bf9a3eb0d5982e4f7f263e947d66a446
Praneeth <pk5110@srmist.edu.in>
2025-10-19 19:16:14 +0000

    add blue/green env vars

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const version = process.env.APP_VERSION || "default";
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Blue-Green Deployment!\n");
+  res.end('Hello from ${version} deployment!\n');
 });
 
 server.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log('App running on port ${port}');
 });
 
